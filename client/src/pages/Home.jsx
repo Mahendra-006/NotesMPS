2be0883c6c52e3be0232f47a95b5437cf339b54e
Mahendra pratap singh <mahendra2002pratap@gmail.com>
2024-10-17 20:56:15 +0530

    first commit

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,210 @@
+import React, {useEffect, useState} from 'react'
+import Navbar from '../components/Navbar'
+import NoteCard from '../components/cards/NoteCard'
+import { IoMdAdd } from "react-icons/io";
+import AddEditNotes from './AddEditNotes';
+import Modal from 'react-modal';
+import { useNavigate } from 'react-router-dom';
+import axiosInstance from '../utils/axiosInstance';
+import Toast from '../components/Toast';
+import EmptyCard from '../components/cards/EmptyCard';
+
+const Home = () => {
+  const [openAddEditModal, setOpenAddEditModal] = useState({
+    isShown: false,
+    type: "add",
+    data: null,
+  });
+
+  const [showToastMessage, setShowToastMessage] = useState({
+    isShown: false,
+    message: "",
+    type: "add",
+  });
+
+  const [allNotes, setAllNotes] = useState([]);
+  const [userInfo, setUserInfo] = useState(null);
+  const [isSearch, setIsSearch] = useState(false);
+
+  const navigate = useNavigate();
+
+  const showToastMsg = (message, type) => {
+    setShowToastMessage({
+      isShown:true,
+      message,
+      type,
+    });
+  };
+
+  const handleCloseToast = () => {
+    setShowToastMessage({
+      isShown:false,
+      message:"",
+    });
+  };
+
+  const handleEdit = (noteDetails) => {
+    setOpenAddEditModal({isShown:true, data: noteDetails, type: "edit"});
+  };
+
+  // get user info
+  const getUserInfo = async () => {
+    try {
+      const response = await axiosInstance.get("/get-user");
+
+      if(response.data && response.data.user) {
+        setUserInfo(response.data.user)
+      }
+    } catch (error) {
+      if(error.response.status === 401) {
+        localStorage.clear();
+        navigate('/login');
+      }
+    }
+  };
+
+  // get all notes
+  const getAllNotes = async () => {
+    try {
+      const response = await axiosInstance.get("/get-all-notes")
+
+      if(response.data && response.data.notes){
+        setAllNotes(response.data.notes);
+      }
+    } catch (error) {
+      console.log("An Unexpected error occurred. Please try again");
+      
+    }
+  }
+
+  //Delete Notes
+  const deleteNote = async (data) => {
+    const noteId = data._id;
+
+    try {
+      const response = await axiosInstance.delete('/delete-note/' + noteId);
+
+      if(response.data && !response.data.error){
+          showToastMsg("Note Deleted Successfully", 'delete')
+          getAllNotes()
+      }
+
+  } catch (error) {
+      if(error.response && error.response.data && error.response.data.message){
+        console.log("An Unexpected error occurred. Please try again");
+      }
+  }
+  }
+
+  //search for a note
+  const onSearchNote = async (query) => {
+    try {
+      const response = await axiosInstance.get("/search-note", {
+        params: {query},
+      });
+
+      if(response.data && response.data.notes){
+        setIsSearch(true);
+        setAllNotes(response.data.notes);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  //update isPinned
+  const updateIsPinned = async (noteData) => {
+    const noteId = noteData._id;
+        try {
+            const response = await axiosInstance.put('/update-note-pinned/' + noteId,{
+              "isPinned": !noteData.isPinned,
+            });
+
+            if(response.data && response.data.note){
+                showToastMsg("Note Updated Successfully")
+                getAllNotes()
+            }
+
+        } catch (error) {
+            console.log(error);
+        }
+  }
+
+  const handleClearSearch = () => {
+    setIsSearch(false);
+    getAllNotes();
+  }
+
+  useEffect(() => {
+    getAllNotes();
+    getUserInfo();
+    return () => {}
+  }, [])
+  
+
+  return (
+    <>
+      <Navbar userInfo={userInfo} onSearchNote={onSearchNote} handleClearSearch={handleClearSearch}/>
+
+      <div className='container mx-auto'>
+        {allNotes.length > 0 ? (<div className='grid grid-cols-3 gap-4 mt-8 '>
+          {allNotes.map((item, index) => (
+            <NoteCard 
+              key={item._id}
+              title={item.title}
+              date={item.createdOn}
+              content={item.content}
+              tags={item.tags} 
+              isPinned={item.isPinned} 
+              onEdit={() => handleEdit(item)}
+              onDelete={() => deleteNote(item)} 
+              onPinNote={() => updateIsPinned(item)}
+            />
+          ))}
+        </div>) : (
+          <EmptyCard message={isSearch? `Oops no note found...` : `Start creating your first note!!! Click on the 'ADD' button to begin your Note making journey. Let's get Started!`}/>
+        )}
+      </div>
+
+      <button className='w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10' 
+        onClick={() => {
+          setOpenAddEditModal({isShown: true, type:"add", data: null});
+        }}>
+      <IoMdAdd 
+        className='text-[32px] text-white'/>
+      </button>
+
+      <Modal 
+            isOpen = {openAddEditModal.isShown}
+            onRequestClose = {() => {}}
+            style={{
+                overlay: {
+                    backgroundColor: "rgba(0,0,0,0.2)",
+                },
+            }}
+            contentLabel=""
+            className="w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 overflow-scroll"
+            >
+                <AddEditNotes 
+                  type={openAddEditModal.type}
+                  noteData={openAddEditModal.data}
+                  onClose={() => {
+                    setOpenAddEditModal({isShown: false, type: "add", data: null});
+                }}
+                getAllNotes={getAllNotes}
+                showToastMsg={showToastMsg}
+                />
+        </Modal>
+
+        <Toast 
+          isShown={showToastMessage.isShown}
+          message={showToastMessage.message}
+          type={showToastMessage.type}
+          onClose={handleCloseToast}
+          />
+
+    </>
+  )
+}
+
+export default Home
