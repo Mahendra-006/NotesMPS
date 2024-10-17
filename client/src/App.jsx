d64b9576fbd4b2d0ebf48505ba8934600a9a5916
Mahendra pratap singh <mahendra2002pratap@gmail.com>
2024-10-17 23:52:58 +0530

    Updated cors and redirected to login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
@@ -9,6 +9,7 @@ const routes = (
     <Routes>
       <Route path="/dashboard" exact element={<Home />} />
       <Route path="/login" exact element={<Login />} />
+      <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/signUp" exact element={<SignUp />} />
     </Routes>
   </Router>
